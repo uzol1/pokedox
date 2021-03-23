1ed56138784deb792e1e6a331312e3f5152408ef
uzol1 <uzol12345@gmail.com>
2021-03-23 19:48:10 +0545

    updated

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react'
+import Pokemon from '../src/Container/Pokemons'
+import Summary from '../src/Components/Summary/Summary'
+import { Route } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <Route path="/" exact component={Pokemon} />
+      <Route path="/pokemon/:id" component={Summary} />
+      <Route path='/pokemon/new' render={() => { return 'this is rendering too' }} />
+    </React.Fragment>
   );
 }
 
---
+++ src/Components/Pokemon/pokemon.js
@@ -0,0 +1,19 @@
+import pokemon from '../../assets/images/quintessential.jpeg'
+import './Pokemon.css'
+import { Link } from 'react-router-dom'
+const Pokemon = (props) => {
+    return (
+        <div className="col-3">
+            <Link to={'/pokemon/' + props.id} >
+                <div className="card" >
+                    <img className="card-img-top" src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${props.id}.png`} alt="Card image cap" />
+                    <div className="card-body">
+                        <h5 className="card-title">{props.name}</h5>
+                    </div>
+                </div>
+            </Link>
+        </div>
+    );
+
+}
+export default Pokemon
